df25fbd38eaacfa2786e6462849b904168040688
Seun <12373359+NatureBoi@users.noreply.github.com>
2019-03-24 21:24:01 -0400

    Tabs are being rendered

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React, { Component } from "react";
+import Card from "./Card";
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+      {props.cards.map((card, i) => (
+        <Card
+          key={i}
+          headline={card.headline}
+          img={card.img}
+          author={card.author}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
